50e462ed51b0784f5edd0736778b4d1b015e481c
Sharon Kaufman <kaufman0106@gmail.com>
2024-02-19 17:51:52 -0500

    feat: Include URI property when adding tracks to the playlist
    - Added mock URI data to each track object in the App component state.
    -Modified the addTrackToPlayList function in the App component to correctly include the URI property for each track when adding them to the playlist.

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ import './App.css';
 function App() {
   const [searchInput, setSearchInput] = useState('');
   const [tracks, setTracks] = useState([
-    { id: 1, name: 'Song 1', artist: 'Artist 1', album: 'Album 1' },
-    { id: 2, name: 'Song 2', artist: 'Artist 2', album: 'Album 2' },
-    { id: 3, name: 'Song 3', artist: 'Artist 3', album: 'Album 3' }
+    { id: 1, name: 'Song 1', artist: 'Artist 1', album: 'Album 1', uri: 'spotify:track:1A2B3C4D5E6F' },
+    { id: 2, name: 'Song 2', artist: 'Artist 2', album: 'Album 2', uri: 'spotify:track:1A2B3C4D5E6G' },
+    { id: 3, name: 'Song 3', artist: 'Artist 3', album: 'Album 3', uri: 'spotify:track:1A2B3C4D5E6H' }
   ]);
   const [playListName, setPlayListName] = useState('My Playlist');
   const [playListTracks, setPlayListTracks] = useState([]);
@@ -20,12 +20,14 @@ function App() {
   };
 
   const addTrackToPlayList = (track) => {
+    // Check if the track already exists in playlist by comparing ids
     if(!playListTracks.some(t => t.id === track.id)) {
-      setPlayListTracks([...playListTracks, track]);
+      setPlayListTracks([...playListTracks, {...track, uri: track.uri}]);
     }
   };
 
   const removeTrackFromPlayList = (trackToRemove) => {
+    // Filter out the track to remove from the playlist based on id
     const updatedPlayListTracks = playListTracks.filter(track => track.id !== trackToRemove.id);
     setPlayListTracks(updatedPlayListTracks);
   };
